2646bb91d95a8ecf0cbfc410fd5e64a116ef4576
ragib <ragibshahriar2002@gmail.com>
2025-12-28 18:36:18 +0600

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,22 @@
-import expresss from 'express';
+import express from 'express';
 import { ENV } from './lib/env.js';
+import path from 'path';
 
-const app = expresss();
-
+const app = express();
+const __dirname = path.resolve();
 console.log('Port:', ENV.PORT);
 
-app.get('/', (req, res) => {
+app.get('/health', (req, res) => {
     res.status(200).json({ message: 'Server is running' });
 })
 
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 app.listen(ENV.PORT, () => {
     console.log('Server is running on port', ENV.PORT);
 });
