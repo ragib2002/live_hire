cbe97ba40eef7bdb38f35f07240356ff346c0461
ragib <ragibshahriar2002@gmail.com>
2025-12-29 16:50:38 +0600

    inngest added

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,7 +1,6 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
-import User from "E:\CSE 470\live_hire\backend\src\models\User.js";
-import { deleteStreamUser, upsertStreamUser } from "./stream.js";
+import User from "../models/User.js";
 
 export const inngest = new Inngest({ id: "talent-iq" });
 
@@ -22,11 +21,7 @@ const syncUser = inngest.createFunction(
 
     await User.create(newUser);
 
-    await upsertStreamUser({
-      id: newUser.clerkId.toString(),
-      name: newUser.name,
-      image: newUser.profileImage,
-    });
+    // todo: do sth
   }
 );
 
@@ -39,7 +34,7 @@ const deleteUserFromDB = inngest.createFunction(
     const { id } = event.data;
     await User.deleteOne({ clerkId: id });
 
-    await deleteStreamUser(id.toString());
+    // todo: do sth else
   }
 );
 
